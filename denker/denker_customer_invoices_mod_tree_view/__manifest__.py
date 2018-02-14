65ca38753f79e06828daa83d9f6d42d32c041599
Benjamín Cruz <bcruz@grupodenker.com>
2018-02-14 16:55:36 -0600

    Se agrega adeudo de la factura en la moneda, BC

---
+++ denker/denker_customer_invoices_mod_tree_view/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Mod Customer Invoices Report Tree view",
+    "version": "10.0.1",
+    'author': 'Servicios Corporativos Denker - BC',
+    'website': 'www.grupodenker.com',
+    'license': 'AGPL-3',
+    "category": "Account Invoice",
+    "description": """ This module add fields to Customer Invoices Tree Report.""",
+    'depends': ['account'],
+    'data': [
+        'views/account_invoice.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+}
