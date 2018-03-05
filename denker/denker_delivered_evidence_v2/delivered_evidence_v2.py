27b08251f11368a06d91692d9f3101d477ea56a7
Benjamín Cruz <bcruz@grupodenker.com>
2018-03-05 12:38:26 -0600

    se quita la opción para los trámites que son de tránsito, o eso intento

--- denker/denker_delivered_evidence_v2/delivered_evidence_v2.py
+++ denker/denker_delivered_evidence_v2/delivered_evidence_v2.py
@@ -49,7 +49,8 @@ class DeliveredEvidence(models.Model):
             for operation in self.pack_operation_ids:
                 if operation.qty_done > operation.product_qty:
                     raise ValidationError(_('No se puede entregar más de lo que está establecido'))
-        if not self.image_medium and (self.location_dest_id.id == 9 or self.location_dest_bool):
+        # if not self.image_medium and (self.location_dest_id.id == 9 or self.location_dest_bool):
+            if not self.image_medium and (self.location_dest_id.id == 9):
             raise ValidationError(_('Please, first attach an image as delivered evidence to continue'))
         return super(DeliveredEvidence,self).do_new_transfer()
 
