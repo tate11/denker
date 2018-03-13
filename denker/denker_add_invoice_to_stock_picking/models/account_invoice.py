459e684bfe4acff56ecce35d35e1b3ff07c28e6b
Benjamín Cruz <bcruz@grupodenker.com>
2018-03-13 13:03:34 -0600

    Modifico módulo de denker_add_invoice_to_stock_picking porque causa error cuando no tiene referencia pero sí nombre

--- denker/denker_add_invoice_to_stock_picking/models/account_invoice.py
+++ denker/denker_add_invoice_to_stock_picking/models/account_invoice.py
@@ -7,17 +7,17 @@ class addto_account_invoice_tracking_reference(models.Model):
 
     @api.multi
     def _compute_tracking_ref(self):
-        print "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL"
+        # print "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL"
         for rec in self:
             print rec
             print rec.id
             M_stock_picking = self.env['stock.picking']
             stock_picking = M_stock_picking.search([('invoice_id', '=', rec.id)])
             rec.carrier_tracking_ref = ""
-            if stock_picking:
+            if stock_picking and reg.carrier_id:
                 for reg in stock_picking:
                     rec.carrier_tracking_ref += reg.carrier_id.name + '-' + reg.carrier_tracking_ref + ' <br>'
-                print rec.carrier_tracking_ref
+                # print rec.carrier_tracking_ref
         return
 
     carrier_tracking_ref = fields.Html('Trackin Ref.', compute='_compute_tracking_ref')
